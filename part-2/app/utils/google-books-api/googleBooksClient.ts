a86c8726c9b1c4a50f3923367184e8757c56f5e3
Le Yuan <leyuan.mun@gmail.com>
2025-03-12 21:08:49 +0800

    Implement book search functionality

---
+++ part-2/app/utils/google-books-api/googleBooksClient.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import ApiError from "@/app/errors/ApiError";
+
+const BASE_URL = process.env.NEXT_PUBLIC_GOOGLE_BOOKS_API_BASE_URL;
+
+export const getBooks = async (query: string, page = 1, maxResults = 10) => {
+  const orderBy = "relevance";
+  const startIndex = (page - 1) * maxResults;
+  const res = await fetch(
+    `${BASE_URL}/volumes?q=${query}&orderBy=${orderBy}&maxResults=${maxResults}&startIndex=${startIndex}`
+  );
+  if (!res.ok) {
+    throw new ApiError();
+  }
+  const data = await res.json();
+  if (!data.totalItems) {
+    return {
+      totalItems: 0,
+      items: [],
+    };
+  } else {
+    return {
+      totalItems: data.totalItems,
+      items: data.items,
+    };
+  }
+};
