62abd74b8b3d941f3e1009fd63cb38afb0224c51
Le Yuan <leyuan.mun@gmail.com>
2025-03-16 00:32:44 +0800

    Clean up styling issues across the app

--- part-2/app/books/[id]/page.tsx
+++ part-2/app/books/[id]/page.tsx
@@ -4,15 +4,17 @@ import Image from "next/image";
 import { getBookDetails } from "@/app/utils/google-books-api/googleBooksServer";
 import NotFoundPage from "@/app/not-found";
 import Link from "next/link";
+import DOMPurify from "isomorphic-dompurify";
 
 export default async function BookDetailPage({
   params,
 }: {
   params: { id: string };
 }) {
+  const { id } = await params;
   let book = null;
   try {
-    book = await getBookDetails(params.id);
+    book = await getBookDetails(id);
   } catch (e) {
     return <NotFoundPage />;
   }
@@ -26,25 +28,31 @@ export default async function BookDetailPage({
   const publishedDate = bookInfo.publishedDate || NA;
   const language = bookInfo.language || NA;
   const previewLink = bookInfo.previewLink || "No preview link available.";
-  const description = bookInfo.description || "No description available.";
+
+  // Sanitize the book description to prevent XSS attacks; fallback to a default message if no description is available.
+  const cleanDescription = bookInfo.description
+    ? DOMPurify.sanitize(bookInfo.description)
+    : "No description available.";
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-bold">{bookInfo.title}</h1>
       <p className="text-gray-600 mb-4">by {author}</p>
-      {bookInfo.imageLinks?.thumbnail ? (
-        <Image
-          src={bookInfo.imageLinks?.thumbnail}
-          alt={bookInfo.title}
-          className="rounded-lg mb-4"
-          width={100}
-          height={100}
-        />
-      ) : (
-        <div className="bg-gray-200 w-[150px] h-[200px] mb-4 flex justify-center items-center text-center text-black">
-          No Image
-        </div>
-      )}
+      <div className="rounded-lg shadow-md w-25 h-40 sm:w-25 sm:h-40 md:w-40 md:h-64 mb-4">
+        {bookInfo.imageLinks?.thumbnail ? (
+          <Image
+            src={bookInfo.imageLinks?.thumbnail}
+            alt={bookInfo.title}
+            className="rounded-lg w-full h-full"
+            width={128}
+            height={205}
+          />
+        ) : (
+          <div className="bg-gray-200 w-full h-full rounded-lg flex justify-center items-center text-black">
+            No Image
+          </div>
+        )}
+      </div>
       <div className="mb-4">
         <p className="font-semibold">Categories:</p>
         <p>{categories}</p>
@@ -67,14 +75,14 @@ export default async function BookDetailPage({
           href={previewLink}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-blue-500"
+          className="text-blue-500 cursor-pointer"
         >
           {previewLink}
         </Link>
       </div>
       <div>
         <p className="font-semibold mb-4">Description:</p>
-        <div dangerouslySetInnerHTML={{ __html: description }} />
+        <div dangerouslySetInnerHTML={{ __html: cleanDescription }} />
       </div>
     </div>
   );
